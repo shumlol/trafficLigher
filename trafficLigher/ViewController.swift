87c6c7e4ef14a7c75b23b74a981c8fa1e1e7770d
Vadim Akhma <shummmmmm@gmail.com>
2023-04-10 21:03:12 +0600

    Initial Commit

---
+++ trafficLigher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  trafficLigher
+//
+//  Created by Вадим Ахматханов on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var swticherLigher: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func switcherLighterPressed(_ sender: UIButton) {
+    }
+    
+}
+
