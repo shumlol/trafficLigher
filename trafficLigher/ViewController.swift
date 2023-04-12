35ac42dafea07a8067c5c108dad9e2d7de986e1e
Vadim Akhma <shummmmmm@gmail.com>
2023-04-12 20:11:52 +0600

    add CGFloat and logic

--- trafficLigher/ViewController.swift
+++ trafficLigher/ViewController.swift
@@ -8,20 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     
-    @IBOutlet var swticherLigher: UIButton!
+    @IBOutlet var switcherLigher: UIButton!
+    
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        switcherLigher.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        print("Размер стороны, доступный в методе viewDidLoad: \(redLight.frame.height)")
     }
 
-    @IBAction func switcherLighterPressed(_ sender: UIButton) {
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        print("Размер стороны, доступный в методе viewWillLayoutSubviews: \(redLight.frame.height)")
+
     }
-    
+    @IBAction func switcherLighterPressed() {
+        if switcherLigher.currentTitle == "START" {
+            switcherLigher.setTitle("NEXT", for: .normal)
+        }
+        
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        }  else if greenLight.alpha == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        } else {
+            redLight.alpha = 1
+                
+        }
+    }
 }
 
 //commit
